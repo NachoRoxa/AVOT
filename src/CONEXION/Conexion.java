a32d0a69f74e534fce995978f0a0ec91ce83a6ae
Seba <Seba@DESKTOP-2B4J2S0>
2018-11-09 01:32:19 -0300

    Borre la wea

--- src/CONEXION/Conexion.java
+++ src/CONEXION/Conexion.java
@@ -6,21 +6,7 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Conexion {  
-//    public static Connection getConexion() {
-//        Connection connection = null;
-//        try {
-//           String driverClassName = "oracle.jdbc.driver.OracleDriver";
-//           String driverUrl="jdbc:oracle:thin:@localhost:1521:XE";
-//           Class.forName(driverClassName);
-//           connection = DriverManager.getConnection(
-//                   driverUrl, "AVOT","paso");
-//        }catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return connection;
-//    }
-    
+public class Conexion {
     Connection cn;
     
     public Connection getConnection(){
