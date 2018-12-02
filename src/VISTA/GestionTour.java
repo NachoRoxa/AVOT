b6862f5dcf7e559fc64e8c57b43a711015cb7736
clabbec <c.labbec@alumnos.duoc.cl>
2018-12-02 16:55:46 -0300

    Merge origin/master
    Conflicts:
    	src/VISTA/GestionCurso.form
    	src/VISTA/GestionCurso.java

--- src/VISTA/GestionTour.java
+++ src/VISTA/GestionTour.java
@@ -10,7 +10,12 @@ import DAO.AgenteDaoImp;
 import DAO.TourDaoImp;
 import DTO.Agente;
 import DTO.Tour;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.stream.IntStream;
 import javax.swing.table.DefaultTableModel;
 
 /**
@@ -18,31 +23,103 @@ import javax.swing.table.DefaultTableModel;
  * @author Seba
  */
 public class GestionTour extends javax.swing.JFrame {
+
     ArrayList<Tour> listaTour;
     ArrayList<Agente> listaAgente;
     Agente agente;
     Conexion obj = new Conexion();
     DefaultTableModel modelo;
     int flag;
+
     /**
      * Creates new form GestionTour
+     *
      * @param admin
      */
-    public GestionTour(int admin)
-    {
+    public GestionTour(int admin) {
         Admin(admin);
         initComponents();
         this.setLocationRelativeTo(null);
         MostrarTours();
         datosComboBox();
-        
-        
+        txtFechaInicio.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                if (!Character.isDigit(e.getKeyChar()) && e.getKeyCode() != KeyEvent.VK_LEFT && e.getKeyCode() != KeyEvent.VK_RIGHT && e.getKeyCode() != KeyEvent.VK_DELETE && e.getKeyCode() != KeyEvent.VK_BACK_SPACE) {
+                    e.consume();
+                }
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (!Character.isDigit(e.getKeyChar()) && e.getKeyCode() != KeyEvent.VK_LEFT && e.getKeyCode() != KeyEvent.VK_RIGHT && e.getKeyCode() != KeyEvent.VK_DELETE && e.getKeyCode() != KeyEvent.VK_BACK_SPACE) {
+                    e.consume();
+                } else {
+                    int[] mesesUp = {1,3,5,7,8,10,12};
+                    int[] mesesDn = {4,6,9,11};
+                    String texto;
+                    texto = txtFechaInicio.getText();
+                    texto = texto.replace("/", "");
+                    String dia;
+                    String mes;
+                    int ano;
+                    int diaInt;
+                    int mesInt;
+                    boolean bisiesto = false;
+                    GregorianCalendar calendar = new GregorianCalendar();
+                    switch (texto.length()) {
+                        case 8:
+                            String[] array = texto.split("");
+                            dia = array[0] + array[1];
+                            diaInt = Integer.parseInt(dia);
+                            mes = array[2] + array[3];
+                            mesInt = Integer.parseInt(mes);
+                            ano = Integer.parseInt(array[4] + array[5] + array[6] + array[7]);
+                            if (calendar.isLeapYear(ano)) {
+                                bisiesto = true;
+                            }
+                            if (Integer.parseInt(mes) > 12) {
+                                mes = "12";
+                            } else if (Integer.parseInt(mes) < 1) {
+                                mes = "01";
+                            }
+                            if (Integer.parseInt(mes) == 2) {
+                                if (bisiesto && Integer.parseInt(dia) > 29) {
+                                    dia = "29";
+                                } else if (Integer.parseInt(dia) > 28) {
+                                    dia = "28";
+                                }
+
+                            } else if (IntStream.of(mesesUp).anyMatch(x -> x == mesInt) && mesInt>31 ) {
+                                dia = "31"; 
+                            } else if (IntStream.of(mesesDn).anyMatch(x -> x == mesInt) && mesInt>30 ) {
+                                dia = "30";
+                            }
+                            txtFechaInicio.setText(dia+"/"+mes+"/"+ano);
+                            
+                        default:
+                            texto = texto.replace("/", "");
+                    }
+
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                if (!Character.isDigit(e.getKeyChar()) && e.getKeyCode() != KeyEvent.VK_LEFT && e.getKeyCode() != KeyEvent.VK_RIGHT && e.getKeyCode() != KeyEvent.VK_DELETE && e.getKeyCode() != KeyEvent.VK_BACK_SPACE) {
+                    e.consume();
+                }
+            }
+        });
+
     }
-    
-    /***
+
+    /**
+     * *
      * Metodo para ver si el usuario posee perfil de administrador.
+     *
      * @param admin
-     * @return 
+     * @return
      */
     public boolean Admin(int admin) {
         this.flag = admin;
@@ -54,8 +131,8 @@ public class GestionTour extends javax.swing.JFrame {
             return false;
         }
     }
-    
-    public void MostrarTours(){
+
+    public void MostrarTours() {
         listaTour = new TourDaoImp().listar();
         modelo = new DefaultTableModel();
         modelo.addColumn("ID");
@@ -87,15 +164,13 @@ public class GestionTour extends javax.swing.JFrame {
         }
     }
 
-    public void datosComboBox()
-    {
+    public void datosComboBox() {
         listaAgente = new AgenteDaoImp().listar();
-        for(Agente agente:listaAgente)
-        {
-            cbAgente.addItem(agente.getNombre()+" "+agente.getApellido_paterno());
+        for (Agente agente : listaAgente) {
+            cbAgente.addItem(agente.getNombre() + " " + agente.getApellido_paterno());
         }
     }
-    
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -126,6 +201,7 @@ public class GestionTour extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         dpFechaInicio = new com.toedter.calendar.JDateChooser();
         dpFechaTermino = new com.toedter.calendar.JDateChooser();
+        txtFechaInicio = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
@@ -255,14 +331,15 @@ public class GestionTour extends javax.swing.JFrame {
                                     .addComponent(jLabel5))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 102, Short.MAX_VALUE))
                             .addGroup(PanelInsertarLayout.createSequentialGroup()
-                                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 86, Short.MAX_VALUE)
                                 .addGap(81, 81, 81)))
                         .addGroup(PanelInsertarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                             .addComponent(txtCosto, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 150, Short.MAX_VALUE)
                             .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 150, Short.MAX_VALUE)
                             .addComponent(cbAgente, javax.swing.GroupLayout.Alignment.LEADING, 0, 150, Short.MAX_VALUE)
                             .addComponent(dpFechaInicio, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(dpFechaTermino, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                            .addComponent(dpFechaTermino, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(txtFechaInicio))))
                 .addContainerGap())
         );
         PanelInsertarLayout.setVerticalGroup(
@@ -293,7 +370,9 @@ public class GestionTour extends javax.swing.JFrame {
                 .addGroup(PanelInsertarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(dpFechaTermino, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 41, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(txtFechaInicio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 12, Short.MAX_VALUE)
                 .addComponent(btnAgregarActividad)
                 .addGap(28, 28, 28))
         );
@@ -332,7 +411,7 @@ public class GestionTour extends javax.swing.JFrame {
     }//GEN-LAST:event_btnInicioActionPerformed
 
     private void btnAgregarActividadActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAgregarActividadActionPerformed
-        
+
     }//GEN-LAST:event_btnAgregarActividadActionPerformed
 
 
@@ -358,5 +437,6 @@ public class GestionTour extends javax.swing.JFrame {
     private javax.swing.JTextField txtActividad;
     private javax.swing.JTextField txtCosto;
     private javax.swing.JTextArea txtDescripcion;
+    private javax.swing.JTextField txtFechaInicio;
     // End of variables declaration//GEN-END:variables
 }
